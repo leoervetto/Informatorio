883a120414a9743292f5bce311e6766355bace5e
leoervetto <leoervetto@gmail.com>
2025-05-12 17:59:50 -0300

    .

--- 20250512/potencia-lista.py
+++ 20250512/potencia-lista.py
@@ -1,4 +1,4 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
-lista_cuadrado = [x**2 for x in lista] 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
+numbers_cuadrado = [x**2 for x in numbers] 
 
-print(lista_cuadrado)  
+print(numbers_cuadrado)  
